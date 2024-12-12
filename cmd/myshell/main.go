1cff44090560cb52e804cbb9dbc520edbff32112
metalpipemomo <bydn.mo@gmail.com>
2024-12-12 09:47:27 -0800

    [any message]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,28 +4,51 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func main() {
+
+	cmds := map[string]*regexp.Regexp{
+		"exit": regexp.MustCompile("exit [0-9]+"),
+	}
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		cmd, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
-		cmd = strings.TrimSpace(cmd)
-
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading command: ", err)
 			return
 		}
 
+		cmd = strings.TrimSpace(cmd)
+
 		if cmd == "" {
-			fmt.Fprintln(os.Stdout, "")
-		} else {
-			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+			continue
 		}
 
+		matched := false
+		for name, regex := range cmds {
+			if match := regex.FindStringSubmatch(cmd); match != nil {
+				matched = true
+				switch name {
+				case "exit":
+					exitCode, parseErr := strconv.Atoi(match[1])
+					if parseErr != nil {
+						fmt.Fprintln(os.Stderr, "Error occured during exit: ", err)
+					} else {
+						os.Exit(exitCode)
+					}
+				}
+			}
+		}
+
+		if !matched {
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+		}
 	}
 }
